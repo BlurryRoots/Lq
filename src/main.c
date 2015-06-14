aa34c0a4024ce28a3af6806399d2e413ffeb46eb
blurryroots <blurryroots@posteo.de>
2015-06-14 12:23:52 +0200

    outline for basic main

--- src/main.c
+++ src/main.c
@@ -1,10 +1,24 @@
-
-#include "lexer.h"
+/**/
+#include <string-library.h>
+#include <lexer.h>
 
 #include <stdio.h>
+#include <locale.h>
 
 int
-main()
-{
+main (void) {
+    setlocale (LC_ALL, "");
+
+    string_t *script;
+    lexer_t lexer;
+
+    script = string_new ();
+    string_copy_cstr (script,
+        L"(func Soßen-köche (Ä b)\n"
+            L"\t(+ Ä b))\n"
+        L"(Soßen-köche 23 42)\n"
+    );
+    wprintf (L"%ls\n", script->data);
+
     return 0;
 }
